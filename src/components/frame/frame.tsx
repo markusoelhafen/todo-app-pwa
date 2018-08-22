658098d411ea4e965d2c4b65b5e246c2e318b539
Markus Ölhafen <markus.oelhafen@sinnerschrader.com>
2018-08-22 22:27:44 +0200

    fix: remove unneeded props

--- src/components/frame/frame.tsx
+++ src/components/frame/frame.tsx
@@ -1,51 +1,22 @@
 import * as React from 'react'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
 export interface FrameProps {
-  maxWidth?: MaxWidth
   children?: React.ReactNode
   centered?: boolean
   className?: string;
 }
 
-export enum MaxWidth {
-  full,
-  medium,
-  half,
-  minimal
-}
-
 const StyledFrame = styled.div`
   display: flex;
   flex-direction: column;
-  ${(props: FrameProps) => props.centered ? `align-items: center` : ``};
-
-  ${(props: FrameProps) => {
-    switch (props.maxWidth) {
-      case MaxWidth.full:
-      default:
-        return css`width: 100%`;
-      case MaxWidth.medium:
-        return css`width: 75%`;
-      case MaxWidth.half:
-        return css`
-          width: 100%;
-          @media (min-width: 720px) {
-            width: 50%;
-          }
-        `;
-      case MaxWidth.minimal:
-        return css`width: 25%`;
-    }
-  }};
+  align-items: center;
+  width: 100%;
+  height: 100%;
 `;
 
 const Frame: React.StatelessComponent<FrameProps> = (props): JSX.Element => (
-  <StyledFrame 
-    centered={props.centered} 
-    maxWidth={props.maxWidth}
-    className={props.className}
-    >
+  <StyledFrame>
     {props.children}
   </StyledFrame>
 )
